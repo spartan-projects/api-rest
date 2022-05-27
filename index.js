5c8523040f59af1289dfbfddbed7769944c80c10
Damian Bruera <brueradamian@gmail.com>
2022-05-27 00:53:58 -0300

    feat: enable mongodb configuration to api-rest

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import 'dotenv/config';
 import userRoutes from './routes/users.js';
 import productRoutes from './routes/products.js';
+import mongoose from 'mongoose';
 
 const port = process.env.PORT || 3000;
+const mongodbPort = process.env.MONGODB_PORT || -1;
+const mongodbHost = process.env.MONGODB_HOST || "localhost";
 const app = express();
 
 app.use(express.json());
@@ -12,7 +15,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/v1", userRoutes);
 app.use("/api/v1", productRoutes);
 
-app.listen(port, () => {
-    console.log(`Server listening on ${port}`);
-})
 
+mongoose.connect(`mongodb://${mongodbHost}:${mongodbPort}`, (err, res) => {
+    if (err) {
+        console.error(`Error while trying to conect with database: ${err}`);
+        return;
+    }
+    console.log("Database connection established");
+
+    app.listen(port, () => {
+        console.log(`Server listening on ${port}`);
+    })
+})
